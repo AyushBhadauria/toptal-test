23cfa8ee0ff22314bb0727c7a6b3bb814d446018
Ayush Bhadauria <bhadauria.ayush1997@gmail.com>
2022-08-08 05:08:03 +0530

    added validations for diet form

--- client/src/component/CreateDiet/CreateDiet.business.ts
+++ client/src/component/CreateDiet/CreateDiet.business.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 export const dietSchema = yup.object({
-    name: yup.string().required('Food name is required'),
-    calories: yup.number().required('Calorie is required'),
+    name: yup.string().required().matches(/^[A-Z a-z]+$/, { message: 'Only characters allowed.' }),
+    calories: yup.number().required().positive('Calories should be greater than 0'),
     consumedAt: yup.date().required(),
     isCheatDiet: yup.boolean()
 });
